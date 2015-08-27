f359ede8745c7eba7341fd12c8e54f5a21682f90
Alexander Pape <a.pape@paneon.de>
2015-08-27 17:18:09 +0200

    Add template and helper

---
+++ fpsmeter_helper.js
@@ -0,0 +1,11 @@
+Template.fpsMeter.helpers({
+    fpsmeter: function() {
+        console.log("fpsmeter called");
+        var meter = new FPSMeter( );
+        return "";
+    }
+});
+
+Template.fpsMeter.rendered = function() {
+    console.log("fpsmeter rendered");
+};
